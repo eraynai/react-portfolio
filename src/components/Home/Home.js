c402067e0eb6b5ac35c3b866e9128b6c8c071dcb
Elli Raynai <elli@elliraynai.com>
2021-08-09 15:37:41 -0400

    added tagline to site

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -3,7 +3,17 @@ import React from 'react';
 export default function Home() {
 	return (
 		<React.Fragment>
-			<h1>Home Page</h1>
+			<section id='about'>
+				<div className='container mx-auto flex px-10 py-20 md:flex-row flex-col items-center'>
+					<div className='lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center'>
+						<h1 className='title-font sm:text-4xl text-3xl mb-4 font-medium text-black'>
+							Hi, I'm Elli.
+							<br className='hidden lg:inline-block' /> I love to build amazing
+							apps.
+						</h1>
+					</div>
+				</div>
+			</section>
 		</React.Fragment>
 	);
 }
