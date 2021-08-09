b4c39bfb260016a4946011064da5d98a765b4e29
Elli Raynai <elli@elliraynai.com>
2021-08-09 09:59:23 -0400

    set up components with react router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
-import './App.css';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Home from './components/Home/Home';
+import About from './components/About/About';
+import SinglePost from './components/SinglePost/SinglePost';
+import Post from './components/Post/Post';
+import Project from './components/Project/Project';
 
 function App() {
-	return <div className='App'></div>;
+	return (
+		<BrowserRouter>
+			<Switch>
+				<Route component={Home} path='/' exact />
+				<Route component={About} path='/about' />
+				<Route component={Post} path='/post' />
+				<Route component={SinglePost} path='/post/:slug' />
+				<Route component={Project} path='/project' />
+			</Switch>
+		</BrowserRouter>
+	);
 }
 
 export default App;
---
+++ src/components/Project/Project.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Project() {
+	return (
+		<React.Fragment>
+			<h1>Project Page</h1>
+		</React.Fragment>
+	);
+}
