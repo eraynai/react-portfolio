86fb9b8f811380719f73849a75e89c6af23d94c2
Elli Raynai <elli@elliraynai.com>
2021-08-12 17:19:05 -0400

    fixed link on homepage

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -24,7 +24,7 @@ export default function Home() {
 						</p>
 						<div className='flex justify-center'>
 							<Link
-								to='/about'
+								to='/contact'
 								className='inline-flex text-white bg-gray-500 border-0 py-2 px-6 focus:outline-none hover:bg-yellow-500 rounded text-lg'
 							>
 								Work With Me
