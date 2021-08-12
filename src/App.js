c31eb1ff6c2ae176d804658a25c7f9ded2568010
Elli Raynai <elli@elliraynai.com>
2021-08-12 17:02:42 -0400

    added google analytics to site

--- src/App.js
+++ src/App.js
@@ -1,19 +1,13 @@
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import React from 'react';
 import Home from './components/Home/Home';
 import About from './components/About/About';
 import Project from './components/Project/Project';
 import NavBar from './components/NavBar/NavBar';
 import Skills from './components/Skills/Skills';
 import ContactForm from './components/ContactForm/ContactForm';
-import ReactGa from 'react-ga4';
-import { useEffect } from 'react';
 
 function App() {
-	useEffect(() => {
-		ReactGa.initialize('G-6GGMB62N37');
-
-		ReactGa.send('pageview');
-	}, []);
 	return (
 		<BrowserRouter>
 			<NavBar />
--- src/index.js
+++ src/index.js
@@ -2,10 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import GA4React from 'ga-4-react';
 
-ReactDOM.render(
-	<React.StrictMode>
-		<App />
-	</React.StrictMode>,
-	document.getElementById('root')
-);
+const ga4react = new GA4React('G-6GGMB62N37');
+
+(async () => {
+	await ga4react
+		.initialize()
+		.then((res) => console.log('Analytics Success'))
+		.catch((err) => console.log('Analytics Failure'))
+		.finally(() => {
+			ReactDOM.render(
+				<React.StrictMode>
+					<App />
+				</React.StrictMode>,
+				document.getElementById('root')
+			);
+		});
+})();
