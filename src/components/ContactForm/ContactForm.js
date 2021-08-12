7f39e5ffad7b681ba559203ddd97593fbee9e626
Elli Raynai <elli@elliraynai.com>
2021-08-12 10:37:47 -0400

    changed the way email is handled

--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -1,33 +1,36 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 export default function Contact() {
-	const [name, setName] = useState('');
-	const [email, setEmail] = useState('');
-	const [message, setMessage] = useState('');
+	// const [name, setName] = useState('');
+	// const [email, setEmail] = useState('');
+	// const [message, setMessage] = useState('');
 
-	function encode(data) {
-		return Object.keys(data)
-			.map(
-				(key) => encodeURIComponent(key) + '=' + encodeURIComponent(data[key])
-			)
-			.join('&');
-	}
+	// function encode(data) {
+	// 	return Object.keys(data)
+	// 		.map(
+	// 			(key) => encodeURIComponent(key) + '=' + encodeURIComponent(data[key])
+	// 		)
+	// 		.join('&');
+	// }
 
-	function handleSubmit(e) {
-		e.preventDefault();
-		fetch('/contact', {
-			method: 'POST',
-			headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-			body: encode({ 'form-name': 'contact', name, email, message }),
-		});
-	}
+	// function handleSubmit(e) {
+	// 	e.preventDefault();
+	// 	fetch('/contact', {
+	// 		method: 'POST',
+	// 		headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+	// 		body: encode({ 'form-name': 'contact', name, email, message }),
+	// 	});
+	// }
 	return (
 		<section id='contact' className='relative'>
 			<div className='container px-5 py-10 mx-auto flex sm:flex-nowrap flex-wrap'>
 				<form
-					netlify
-					name='contact'
-					onSubmit={handleSubmit}
+					// netlify
+					action='https://formsubmit.co/elli@elliraynai.com'
+					method='POST'
+					target='_blank'
+					// name='contact'
+					// onSubmit={handleSubmit}
 					className='lg:w-1/3 md:w-1/2 flex flex-col md:m-auto w-full md:py-8 mt-8 md:mt-0'
 				>
 					<h2 className='text-white sm:text-4xl text-3xl mb-1 font-medium title-font'>
@@ -46,7 +49,9 @@ export default function Contact() {
 							id='name'
 							name='name'
 							className='w-full bg-gray-800 rounded border border-gray-700 focus:border-yellow-500 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out'
-							onChange={(e) => setName(e.target.value)}
+							placeholder='Full Name'
+							required
+							// onChange={(e) => setName(e.target.value)}
 						/>
 					</div>
 					<div className='relative mb-4'>
@@ -58,7 +63,9 @@ export default function Contact() {
 							id='email'
 							name='email'
 							className='w-full bg-gray-800 rounded border border-gray-700 focus:border-yellow-500 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out'
-							onChange={(e) => setEmail(e.target.value)}
+							placeholder='Email Address'
+							required
+							// onChange={(e) => setEmail(e.target.value)}
 						/>
 					</div>
 					<div className='relative mb-4'>
@@ -72,7 +79,8 @@ export default function Contact() {
 							id='message'
 							name='message'
 							className='w-full bg-gray-800 rounded border border-gray-700 focus:border-yellow-500 h-32 text-base outline-none text-gray-100 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out'
-							onChange={(e) => setMessage(e.target.value)}
+							required
+							// onChange={(e) => setMessage(e.target.value)}
 						/>
 					</div>
 					<button
