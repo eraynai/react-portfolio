3118ced5bf59c58288812e9d2a8e86fb6576d3da
Elli Raynai <elli@elliraynai.com>
2021-08-13 02:03:04 -0400

    fixed navigation bar

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -9,7 +9,7 @@ export default function Navbar({ fixed }) {
 	return (
 		<React.Fragment>
 			<header className='text-white bg-yellow-500 body-font w-full'>
-				<div class='container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center'>
+				<div class='container mx-auto flex flex-wrap p-3 flex-col md:flex-row items-center'>
 					<nav className='flex lg:w-2/5 flex-wrap items-center text-base md:ml-auto mt-2'>
 						<NavLink
 							className='mr-5 cursor-pointer border-b border-transparent hover:opacity-75 text-white font-bold'
