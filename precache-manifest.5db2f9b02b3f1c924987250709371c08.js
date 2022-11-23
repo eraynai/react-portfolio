113cc7d081b4c92345ae02e6ad133dc2fe897885
eraynai <elli@elliraynai.com>
2022-11-23 13:10:30 -0500

    Updates

---
+++ precache-manifest.5db2f9b02b3f1c924987250709371c08.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "82c8a5252bc21dddbcde",
+    "url": "/static/css/main.ffa2c219.chunk.css"
+  },
+  {
+    "revision": "82c8a5252bc21dddbcde",
+    "url": "/static/js/main.82c8a525.chunk.js"
+  },
+  {
+    "revision": "5c7be2fa98e6ee92dba7",
+    "url": "/static/js/1.5c7be2fa.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5095c40cd479b900d220f27049a16008",
+    "url": "/static/media/elli_pic.5095c40c.svg"
+  },
+  {
+    "revision": "bd8d5dd7aec27ec078868fa59787ce06",
+    "url": "/static/media/elli_logo_v2.bd8d5dd7.svg"
+  },
+  {
+    "revision": "54a15a9f678fac59f68bd70cc61b9746",
+    "url": "/static/media/elli_raynai_cv_october_2022.54a15a9f.pdf"
+  },
+  {
+    "revision": "1c6e1f53d372c0d4284140b4bd9724a5",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/react-portfolio/precache-manifest.2e00aee27e39436bea8d7b12210cda27.js"
+  "/precache-manifest.5db2f9b02b3f1c924987250709371c08.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/react-portfolio/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
