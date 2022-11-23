8caed4870bea274b832a266fbaccb7cb980c2ae1
eraynai <elli@elliraynai.com>
2022-11-23 12:53:03 -0500

    Updates

---
+++ precache-manifest.2e00aee27e39436bea8d7b12210cda27.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "82c8a5252bc21dddbcde",
+    "url": "/react-portfolio/static/css/main.ffa2c219.chunk.css"
+  },
+  {
+    "revision": "82c8a5252bc21dddbcde",
+    "url": "/react-portfolio/static/js/main.82c8a525.chunk.js"
+  },
+  {
+    "revision": "5c7be2fa98e6ee92dba7",
+    "url": "/react-portfolio/static/js/1.5c7be2fa.chunk.js"
+  },
+  {
+    "revision": "f8926043fa4459ec0101",
+    "url": "/react-portfolio/static/js/runtime~main.f8926043.js"
+  },
+  {
+    "revision": "5095c40cd479b900d220f27049a16008",
+    "url": "/react-portfolio/static/media/elli_pic.5095c40c.svg"
+  },
+  {
+    "revision": "bd8d5dd7aec27ec078868fa59787ce06",
+    "url": "/react-portfolio/static/media/elli_logo_v2.bd8d5dd7.svg"
+  },
+  {
+    "revision": "54a15a9f678fac59f68bd70cc61b9746",
+    "url": "/react-portfolio/static/media/elli_raynai_cv_october_2022.54a15a9f.pdf"
+  },
+  {
+    "revision": "12a5d86d32dbcd3d6abae41d8711c97f",
+    "url": "/react-portfolio/index.html"
+  }
+];
