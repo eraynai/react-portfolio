b24cd129e94a4b1891e6bc9d5f24443da45b9eaf
eraynai2 <elli@elliraynai.com>
2023-09-21 06:15:33 -0400

    cleared the contents of revised-portfolio

--- src/components/Line.jsx
+++
@@ -1,8 +0,0 @@
-const Line = () => {
-  return (
-    <svg width="20px" height="5" xmlns="http://www.w3.org/2000/svg">
-      <rect x="0" y="0" width="20px" height="5" fill="white" />
-    </svg>
-  );
-};
-export default Line;
