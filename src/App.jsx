e176fdcdee594be2dbb1d969866f33d7a3d5addd
eraynai2 <elli@elliraynai.com>
2023-09-21 17:14:34 -0400

    set up project

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,13 @@
+import Hero from "./components/Hero";
+
 function App() {
-  return <>test</>;
+  return (
+    <>
+      <main className="flex min-h-screen flex-col bg-[#121212]">
+        <Hero />
+      </main>
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/Hero.jsx
@@ -0,0 +1,8 @@
+const Hero = () => {
+  return (
+    <>
+      <h1>Portfolio Website</h1>
+    </>
+  );
+};
+export default Hero;
