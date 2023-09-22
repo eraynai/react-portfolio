59f8687aae7e83bec954b014d025691767519a78
eraynai2 <elli@elliraynai.com>
2023-09-22 06:00:26 -0400

    added lorem ipsum

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Hero from "./components/Hero";
 function App() {
   return (
     <>
-      <main className="flex min-h-screen flex-col bg-[#121212]">
+      <main className="flex min-h-screen flex-col bg-[#121212] container mx-auto px-12 py-4">
         <Hero />
       </main>
     </>
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,20 @@
 const Hero = () => {
   return (
-    <>
-      <h1>Portfolio Website</h1>
-    </>
+    <section>
+      <div className="grid grid-cols-1 lg:grid-cols-12">
+        <div className="col-span-7 place-self-center">
+          <h1 className="text-white mb-4 lg:text-6xl text-4xl font-extrabold">
+            Hello, I'm Elli
+          </h1>
+          <p className="text-[#adb7be] text-lg lg:text-xl">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur,
+            maxime, quae dicta non corporis molestias et impedit hic eligendi
+            voluptate mollitia commodi voluptatem obcaecati. Excepturi inventore
+            consequatur quia delectus tempore.
+          </p>
+        </div>
+      </div>
+    </section>
   );
 };
 export default Hero;
