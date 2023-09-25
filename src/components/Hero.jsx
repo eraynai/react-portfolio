ceec723adf14cf0bdf874d7a1e28ab7cb41b74a1
eraynai2 <elli@elliraynai.com>
2023-09-24 22:52:44 -0400

    added tag line

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -26,7 +26,7 @@ const Hero = () => {
             />
           </h1>
           <p className="text-[#adb7be] text-base sm:text-lg mb-6 lg:text-xl sm:mr-5">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Coding with Passion, Developing with Purpose.
           </p>
           <div>
             <button className="px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-200 text-white">
